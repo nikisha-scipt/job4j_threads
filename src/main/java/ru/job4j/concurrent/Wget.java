cf523b691d8377fa780c5ee577217baf5f0ed955
nikisha-script <nikisha_script@mail.ru>
2022-06-21 21:55:39 +0300

    1.2. Режим ожидания. [#231217]

---
+++ src/main/java/ru/job4j/concurrent/Wget.java
@@ -0,0 +1,23 @@
+package ru.job4j.concurrent;
+
+public class Wget {
+
+    public static void main(String[] args) {
+        Thread thread = new Thread(
+                () -> { }
+        );
+        thread.start();
+        while (thread.getState() != Thread.State.TERMINATED) {
+            for (int i = 0; i <= 100; i++) {
+                try {
+                    System.out.print("\rLoading : " + i  + "%");
+                    Thread.sleep(10);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        System.out.printf("%n%s ended", Thread.currentThread().getName());
+    }
+
+}
