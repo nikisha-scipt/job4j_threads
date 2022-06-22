6c1d84f99cee22b522804cfa3aa79fd642e76c15
nikisha-script <nikisha_script@mail.ru>
2022-06-22 15:47:15 +0300

    1. Visibility. Общий ресурс вне критической секции [#1102]

---
+++ src/main/java/ru/job4j/concurrent/synchronization/ParseFile.java
@@ -0,0 +1,49 @@
+package ru.job4j.concurrent.synchronization;
+
+import java.io.*;
+import java.util.function.Predicate;
+
+public final class ParseFile {
+
+    private final File file;
+
+    public ParseFile(File file) {
+        this.file = file;
+    }
+
+    public synchronized String getContent() {
+        return content(character -> true);
+    }
+
+    public synchronized String getContentWithoutUnicode() {
+        return content(character -> character < 0x80);
+    }
+
+    public synchronized String content(Predicate<Character> filter) {
+        StringBuilder result = new StringBuilder();
+        try (BufferedInputStream input = new BufferedInputStream(new FileInputStream(file))) {
+            int data;
+            while ((data = input.read()) > 0)  {
+                if (filter.test((char) data)) {
+                    result.append((char) data);
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return result.toString();
+    }
+
+    private final class SaveFile {
+        public synchronized void saveContent(String content) {
+            try (BufferedOutputStream o = new BufferedOutputStream(new FileOutputStream(file))) {
+                for (int i = 0; i < content.length(); i++) {
+                    o.write(content.charAt(i));
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+}
