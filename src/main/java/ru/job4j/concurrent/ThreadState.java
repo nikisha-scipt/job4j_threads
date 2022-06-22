b5afbd28db69ffcd9b9e262f3d59069301833e4a
nikisha-script <nikisha_script@mail.ru>
2022-06-22 08:08:36 +0300

    1.1. Состояние нити. [#229175 #315090] (reload)

--- src/main/java/ru/job4j/concurrent/ThreadState.java
+++ src/main/java/ru/job4j/concurrent/ThreadState.java
@@ -3,13 +3,12 @@ package ru.job4j.concurrent;
 public class ThreadState {
 
     public static void main(String[] args) {
-        Thread first = new Thread();
-        Thread second = new Thread();
+        Thread first = new Thread(() -> System.out.println(Thread.currentThread().getName()));
+        Thread second = new Thread(() -> System.out.println(Thread.currentThread().getName()));
         first.start();
         second.start();
         while (first.getState() != Thread.State.TERMINATED || second.getState() != Thread.State.TERMINATED) {
-            System.out.println(first.getName());
-            System.out.println(second.getName());
+            System.out.println("Work pool");
         }
         System.out.println(Thread.currentThread().getName() + " ended");
     }
