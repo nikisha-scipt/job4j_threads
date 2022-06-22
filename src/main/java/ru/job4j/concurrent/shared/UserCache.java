e1a26d5c331e6a6fe55b6f139f16eb9d62b2a916
nikisha-script <nikisha_script@mail.ru>
2022-06-22 15:22:04 +0300

    4. Thread без общих ресурсов [#267919]

---
+++ src/main/java/ru/job4j/concurrent/shared/UserCache.java
@@ -0,0 +1,59 @@
+package ru.job4j.concurrent.shared;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class UserCache {
+
+    private final ConcurrentHashMap<Integer, User> users = new ConcurrentHashMap<>();
+    private final AtomicInteger id = new AtomicInteger();
+
+    public void add(User user) {
+        users.put(id.incrementAndGet(), User.of(user.getName()));
+    }
+
+    public User findById(int id) {
+        return User.of(users.get(id).getName());
+    }
+
+    public List<User> findAll() {
+        return new ArrayList<>(users.values());
+    }
+
+    private static class User {
+
+        private int id;
+        private String name;
+
+        public static User of(String name) {
+            User user = new User();
+            user.name = name;
+            return user;
+        }
+
+        public int getId() {
+            return id;
+        }
+
+        public void setId(int id) {
+            this.id = id;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        @Override
+        public String toString() {
+            return "User{" + "id=" + id + ", name='" + name + '\'' + '}';
+        }
+    }
+
+}
+
