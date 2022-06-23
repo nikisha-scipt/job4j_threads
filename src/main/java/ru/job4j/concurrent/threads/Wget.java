6fcfbf0d4bec908602448cb4f6f134871edda79a
nikisha-script <nikisha_script@mail.ru>
2022-06-23 09:43:15 +0300

    4. Скачивание файла с ограничением. [#144271 #315234] (reload)

--- src/main/java/ru/job4j/concurrent/threads/Wget.java
+++ src/main/java/ru/job4j/concurrent/threads/Wget.java
@@ -9,22 +9,26 @@ import java.net.URL;
 public class Wget implements Runnable {
 
     private final String url;
+    private final String file;
     private final int speed;
 
-    public Wget(String url, int speed) {
+    public Wget(String url, int speed, String file) {
         this.url = url;
         this.speed = speed;
+        this.file = file;
     }
 
     @Override
     public void run() {
         try (BufferedInputStream in = new BufferedInputStream(new URL(url).openStream());
-             BufferedOutputStream out = new BufferedOutputStream(new FileOutputStream("pom_temp.xml"))) {
-            byte[] downloadBuffer = new byte[speed];
+             BufferedOutputStream out = new BufferedOutputStream(new FileOutputStream(file))) {
+            byte[] downloadBuffer = new byte[1024];
             int byteCount;
             while ((byteCount = in.read(downloadBuffer, 0, 1024)) != -1) {
                 out.write(downloadBuffer, 0, byteCount);
-                Thread.sleep(1000);
+                if (System.currentTimeMillis() < speed) {
+                    Thread.sleep(1000);
+                }
             }
             System.out.printf("%s successfully download%n", Thread.currentThread().getName());
         } catch (IOException | InterruptedException e) {
@@ -32,11 +36,53 @@ public class Wget implements Runnable {
         }
     }
 
+    private static class Validation {
+
+        private final String[] args;
+
+        public Validation(String[] args) {
+            this.args = args;
+        }
+
+        private boolean checkSize() {
+            return args.length == 3;
+        }
+
+        private boolean checkUrl() {
+            return args[0].startsWith("http");
+        }
+
+        private boolean checkSpeed() {
+            return Integer.parseInt(args[1]) != 0;
+        }
+
+        private boolean checkFile() {
+            return args[2].contains(".");
+        }
+
+        public boolean isValid() {
+            try {
+                checkSize();
+                checkUrl();
+                checkSpeed();
+                checkFile();
+            } catch (IllegalArgumentException e) {
+                throw new IllegalArgumentException("Format: http://file.txt 5 file.txt");
+            }
+            return true;
+        }
+
+    }
+
     public static void main(String[] args) throws InterruptedException {
-        String url = args[0];
-        int speed = Integer.parseInt(args[1]);
-        Thread wget = new Thread(new Wget(url, speed));
-        wget.start();
-        wget.join();
+        Validation valid = new Validation(args);
+        if (valid.isValid()) {
+            String url = args[0];
+            int speed = Integer.parseInt(args[1]);
+            String file = args[2];
+            Thread wget = new Thread(new Wget(url, speed, file));
+            wget.start();
+            wget.join();
+        }
     }
 }
