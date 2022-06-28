2850e4ecfef56726a91f942ea6808a4dc8b731da
nikisha-script <nikisha_script@mail.ru>
2022-06-28 18:16:06 +0300

    3. Junit тест для блокирующей очереди. [#68589]

--- src/test/java/ru/job4j/concurrent/wnn/SimpleBlockingQueueTest.java
+++ src/test/java/ru/job4j/concurrent/wnn/SimpleBlockingQueueTest.java
@@ -3,6 +3,10 @@ package ru.job4j.concurrent.wnn;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.stream.IntStream;
+
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 
@@ -10,10 +14,12 @@ import static org.hamcrest.Matchers.is;
 public class SimpleBlockingQueueTest {
 
     private SimpleBlockingQueue<Integer> queue;
+    private CopyOnWriteArrayList<Integer> buffer;
 
     @Before
     public void init() {
         this.queue = new SimpleBlockingQueue<>(5);
+        this.buffer = new CopyOnWriteArrayList<>();
     }
 
     @Test
@@ -37,6 +43,36 @@ public class SimpleBlockingQueueTest {
         producer.join();
         consumer.join();
         assertThat(queue.getSize(), is(0));
+    }
+
+    @Test
+    public void whenFetchAllThenGetIt() throws InterruptedException {
+        Thread producer = new Thread(() -> {
+            for (int i = 0; i < 5; i++) {
+                try {
+                    queue.offer(i);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        producer.start();
+
+        Thread consumer = new Thread(() -> {
+            while (queue.getSize() != 0 || !Thread.currentThread().isInterrupted()) {
+                try {
+                    buffer.add(queue.poll());
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    Thread.currentThread().interrupt();
+                }
+            }
+        });
+        consumer.start();
+        producer.join();
+        consumer.interrupt();
+        consumer.join();
+        assertThat(buffer, is(List.of(0, 1, 2, 3, 4)));
     }
 
 }
